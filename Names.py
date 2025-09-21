e6a4441508c074d8bfc566edbd0b2b3cf865c8cd
Sriram Subramanian <166926838+Srikyz@users.noreply.github.com>
2025-09-21 12:43:33 +0530

    Host name Added

--- Names.py
+++ Names.py
@@ -1,3 +1,3 @@
 print("Hi Everyone who are going to contribute to this code")
-print("I want you all to write down your name and your roll number in a signle print statement")
-print("Hi My Name is Sriram")
+print("I want you all to write down your name, your roll number, and your GitHub profile link in a single print statement")
+print("Hi My Name is Sriram, 23EEB0A08, https://github.com/Srikyz")
